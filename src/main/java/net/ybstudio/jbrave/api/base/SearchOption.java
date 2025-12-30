2158be09c17e5e538ef8ec146d1cc591b25921b4
Yoham Gabriel B. @Urbine <yohamg@programmer.net>
2025-12-30 17:47:14 +0700

    feat(api-base): add SearchOption interface to model query-level search options consistently
    Signed-off-by: Yoham Gabriel B. @Urbine <yohamg@programmer.net>

---
+++ src/main/java/net/ybstudio/jbrave/api/base/SearchOption.java
@@ -0,0 +1,38 @@
+/*
+ * JBrave
+ *
+ * Copyright 2025-2026 Yoham Gabriel Barboza B. (YGBStudio)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package net.ybstudio.jbrave.api.base;
+
+/**
+ * An interface representing a search option.
+ *
+ * <p>This interface extends {@link BraveAPIConstant} and is implemented by enums that represent
+ * search options.
+ *
+ * <p>A search option is a parameter that can be used to modify the behavior of the API requests. In
+ * JBrave's base model, all search options appear in the query parameters of requests and that is
+ * why identifiers, verticals, filters and options implement this interface by default.
+ *
+ * @see BraveAPIConstant
+ * @author Yoham Gabriel Barboza B. (YGBStudio)
+ */
+public non-sealed interface SearchOption extends BraveAPIConstant {
+  String UNITS = "units";
+}
