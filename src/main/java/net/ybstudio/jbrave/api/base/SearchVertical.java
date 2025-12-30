a1dd884cffd1339421c110673558f3043fceb43c
Yoham Gabriel B. @Urbine <yohamg@programmer.net>
2025-12-30 17:52:25 +0700

    feat(api-base): add SearchVertical to model API search verticals consistently
    Signed-off-by: Yoham Gabriel B. @Urbine <yohamg@programmer.net>

---
+++ src/main/java/net/ybstudio/jbrave/api/base/SearchVertical.java
@@ -0,0 +1,39 @@
+/*
+ * JBrave
+ *
+ * Copyright 2025-2026 Yoham Gabriel Barboza B. (YGBStudio)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package net.ybstudio.jbrave.api.base;
+
+/**
+ * An interface representing a search vertical.
+ *
+ * <p>This interface extends {@link BraveAPIConstant} and is implemented by enums that represent
+ * search verticals.
+ *
+ * <p>Each search vertical has a {@link #value()} method that is used to get the parameter value for
+ * the API requests. All verticals are followed by the {@link #SEARCH_PATH} string, and it is
+ * included here for convenience and readability as it is used in the implementation of the <br>
+ * {@link #urlParam()} method by default.
+ *
+ * @see BraveAPIConstant
+ * @author Yoham Gabriel Barboza B. (YGBStudio)
+ */
+public non-sealed interface SearchVertical extends BraveAPIConstant {
+  String SEARCH_PATH = "search";
+}
