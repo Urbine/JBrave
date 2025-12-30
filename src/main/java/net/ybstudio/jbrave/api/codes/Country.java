c1d247fb258d63c364e8b46b193470f1540337ec
Yoham Gabriel B. @Urbine <yohamg@programmer.net>
2025-12-30 17:54:16 +0700

    feat(api-codes): add Country enum to provide official country codes for Brave Search queries
    Signed-off-by: Yoham Gabriel B. @Urbine <yohamg@programmer.net>

---
+++ src/main/java/net/ybstudio/jbrave/api/codes/Country.java
@@ -0,0 +1,88 @@
+/*
+ * JBrave
+ *
+ * Copyright 2025-2026 Yoham Gabriel Barboza B. (YGBStudio)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package net.ybstudio.jbrave.api.codes;
+
+import net.ybstudio.jbrave.api.base.CountryIdentifier;
+
+/**
+ * Enumeration representing a country supported by the Brave Search API.
+ *
+ * <p>This enumeration implements the {@link CountryIdentifier} interface, providing methods for URL
+ * parameter generation.
+ *
+ * @see CountryIdentifier
+ * @author Yoham Gabriel Barboza B. (YGBStudio)
+ */
+public enum Country implements CountryIdentifier {
+  ARGENTINA("AR"),
+  AUSTRALIA("AU"),
+  AUSTRIA("AT"),
+  BELGIUM("BE"),
+  BRAZIL("BR"),
+  CANADA("CA"),
+  CHILE("CL"),
+  DENMARK("DK"),
+  FINLAND("FI"),
+  FRANCE("FR"),
+  GERMANY("DE"),
+  GREECE("GR"),
+  HONG_KONG("HK"),
+  INDIA("IN"),
+  INDONESIA("ID"),
+  ITALY("IT"),
+  JAPAN("JP"),
+  KOREA("KR"),
+  MALAYSIA("MY"),
+  MEXICO("MX"),
+  NETHERLANDS("NL"),
+  NEW_ZEALAND("NZ"),
+  NORWAY("NO"),
+  CHINA("CN"),
+  POLAND("PL"),
+  PORTUGAL("PT"),
+  PHILIPPINES("PH"),
+  RUSSIA("RU"),
+  SAUDI_ARABIA("SA"),
+  SOUTH_AFRICA("ZA"),
+  SPAIN("ES"),
+  SWEDEN("SE"),
+  SWITZERLAND("CH"),
+  TAIWAN("TW"),
+  TURKEY("TR"),
+  UNITED_KINGDOM("GB"),
+  UNITED_STATES("US");
+
+  private final String value;
+
+  Country(String value) {
+    this.value = value;
+  }
+
+  @Override
+  public String value() {
+    return value;
+  }
+
+  @Override
+  public String urlParam() {
+    return CountryIdentifier.urlParam(this);
+  }
+}
