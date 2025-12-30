90f0265795cdf86bfa62ace2e7072b96bbcc93f1
Yoham Gabriel B. @Urbine <yohamg@programmer.net>
2025-12-30 17:50:36 +0700

    feat(api-base): introduce SearchFilter to formalize filter-related query parameters
    Signed-off-by: Yoham Gabriel B. @Urbine <yohamg@programmer.net>

---
+++ src/main/java/net/ybstudio/jbrave/api/base/SearchFilter.java
@@ -0,0 +1,36 @@
+/*
+ * JBrave
+ *
+ * Copyright 2025-2026 Yoham Gabriel Barboza B. (YGBStudio)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package net.ybstudio.jbrave.api.base;
+
+/**
+ * An interface representing a search filter.
+ *
+ * <p>This interface is extended by enums that represent search filters. {@link SearchFilter} also
+ * offers fields that are used to generate URL parameter combinations for the API requests.
+ *
+ * @see BraveAPIConstant
+ * @author Yoham Gabriel Barboza B. (YGBStudio)
+ */
+public non-sealed interface SearchFilter extends BraveAPIConstant, SearchOption {
+  String SAFESEARCH = "safesearch";
+  String FRESHNESS = "freshness";
+  String RESULT_FILTER = "result_filter";
+}
