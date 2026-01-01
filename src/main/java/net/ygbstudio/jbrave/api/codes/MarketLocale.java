9d4b65a028c584caae61911f4ef39d46d04e8881
Yoham Gabriel B. @Urbine <yohamg@programmer.net>
2026-01-01 19:11:13 +0700

    feat(api-codes): implement SearchOptionCarrier conversion for MarketLocale enum to unify locale option handling chore(packaging): rename base package from net.ybstudio to net.ygbstudio to reflect correct organization ownership
    Signed-off-by: Yoham Gabriel B. @Urbine <yohamg@programmer.net>

--- src/main/java/net/ygbstudio/jbrave/api/codes/MarketLocale.java
+++ src/main/java/net/ygbstudio/jbrave/api/codes/MarketLocale.java
@@ -18,9 +18,14 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-package net.ybstudio.jbrave.api.codes;
+package net.ygbstudio.jbrave.api.codes;
 
-import net.ybstudio.jbrave.api.base.RegionLocaleIdentifier;
+import net.ygbstudio.jbrave.api.base.RegionLocaleIdentifier;
+import net.ygbstudio.jbrave.api.base.model.SearchOptionCarrier;
+import net.ygbstudio.jbrave.api.options.BraveSearchOption;
+import org.jetbrains.annotations.Contract;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Unmodifiable;
 
 /**
  * Enumeration representing a market locale supported by the Brave Search API.
@@ -84,7 +89,13 @@ public enum MarketLocale implements RegionLocaleIdentifier {
   }
 
   @Override
-  public String urlParam() {
+  public @NotNull String urlParam() {
     return RegionLocaleIdentifier.urlParam(this);
+  }
+
+  @Contract(" -> new")
+  @Override
+  public @NotNull @Unmodifiable SearchOptionCarrier<String> toSearchOption() {
+    return BraveSearchOption.of(this, value);
   }
 }
