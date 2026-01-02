0f60354ef1fac6f3f5a37665dc7d2af332c0e83a
Yoham Gabriel B. @Urbine <yohamg@programmer.net>
2026-01-02 17:48:55 +0700

    feat(exceptions): add AbsentSearchVerticalException to signal missing search verticals in URL builders docs(exceptions): add Javadoc explaining when AbsentSearchVerticalException is thrown and its relation to AbstractQueryUrlBuilder chore(license): add Apache-2.0 license header to AbsentSearchVerticalException.java
    Signed-off-by: Yoham Gabriel B. @Urbine <yohamg@programmer.net>

---
+++ src/main/java/net/ygbstudio/jbrave/api/exceptions/AbsentSearchVerticalException.java
@@ -0,0 +1,43 @@
+/*
+ * JBrave
+ *
+ * Copyright 2025-2026 Yoham Gabriel Barboza B. (YGBStudio)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package net.ygbstudio.jbrave.api.exceptions;
+
+import java.util.function.Supplier;
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * Exception thrown when a search vertical is absent in a URL query builder.
+ *
+ * <p>URL query builders must call {@code addInstanceVertical(SearchVertical)} before building if
+ * the implementation inherits from <br>
+ * {@link net.ygbstudio.jbrave.api.base.builders.AbstractQueryUrlBuilder}.
+ *
+ * @author Yoham Gabriel Barboza B. (YGBStudio)
+ */
+public class AbsentSearchVerticalException extends RuntimeException {
+  public AbsentSearchVerticalException(String message) {
+    super(message);
+  }
+
+  public AbsentSearchVerticalException(@NotNull Supplier<String> message) {
+    super(message.get());
+  }
+}
