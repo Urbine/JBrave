a477ba85b67f997083049b8359b77b0e32012484
Yoham Gabriel B. @Urbine <yohamg@programmer.net>
2026-01-02 17:52:21 +0700

    feat(builders): add BraveImageQueryBuilder to construct type-safe, immutable image search queries for the Brave API docs(builders): add Javadoc for BraveImageQueryBuilder explaining usage, builder creation, and available query options chore(license): add Apache-2.0 license header to BraveImageQueryBuilder.java
    Signed-off-by: Yoham Gabriel B. @Urbine <yohamg@programmer.net>

---
+++ src/main/java/net/ygbstudio/jbrave/api/builders/BraveImageQueryBuilder.java
@@ -0,0 +1,113 @@
+/*
+ * JBrave
+ *
+ * Copyright 2025-2026 Yoham Gabriel Barboza B. (YGBStudio)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package net.ygbstudio.jbrave.api.builders;
+
+import net.ygbstudio.jbrave.api.base.CountryIdentifier;
+import net.ygbstudio.jbrave.api.base.LanguageIdentifier;
+import net.ygbstudio.jbrave.api.base.builders.AbstractQueryUrlBuilder;
+import net.ygbstudio.jbrave.api.filters.SafeSearch;
+import net.ygbstudio.jbrave.api.options.SearchOptions;
+import net.ygbstudio.jbrave.api.verticals.BraveResource;
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * A builder for constructing Brave Image Search API queries.
+ *
+ * <p>This class is specifically designed for constructing Brave Image Search API queries, and is
+ * not intended to be instantiated directly. Instead, use the {@link #builder()} method to create a
+ * new instance of the builder.
+ *
+ * <p>The builder is immutable, reusable and type-safe.
+ *
+ * @author Yoham Gabriel Barboza B. (YGBStudio)
+ */
+public final class BraveImageQueryBuilder extends AbstractQueryUrlBuilder<BraveImageQueryBuilder> {
+  private BraveImageQueryBuilder() {}
+
+  /**
+   * Returns a new instance of {@link BraveImageQueryBuilder}
+   *
+   * @return a new instance of {@link BraveImageQueryBuilder}
+   */
+  public static BraveImageQueryBuilder builder() {
+    return new BraveImageQueryBuilder().addInstanceVertical(BraveResource.IMAGE).clear();
+  }
+
+  /**
+   * Adds a search term to the URL query.
+   *
+   * @param queryTerm the search term to add
+   * @return the current instance of the builder
+   */
+  public BraveImageQueryBuilder query(String queryTerm) {
+    return addQueryTerm(queryTerm);
+  }
+
+  /**
+   * Adds the search language option to the URL query.
+   *
+   * @param searchLanguage the search language to set
+   * @return the current instance of the builder
+   */
+  public BraveImageQueryBuilder language(@NotNull LanguageIdentifier searchLanguage) {
+    return addOptionCarrier(searchLanguage.toSearchOption());
+  }
+
+  /**
+   * Adds a safe search option to the URL query.
+   *
+   * @param safeSearch a {@link SafeSearch} enum value specifying the safe search level
+   * @return the current instance of the builder
+   */
+  public BraveImageQueryBuilder safeSearch(@NotNull SafeSearch safeSearch) {
+    return addOptionCarrier(safeSearch.toSearchOption());
+  }
+
+  /**
+   * Adds the country option to the URL query.
+   *
+   * @param country the country identifier to set
+   * @return the current instance of the builder
+   */
+  public BraveImageQueryBuilder country(@NotNull CountryIdentifier country) {
+    return addOptionCarrier(country.toSearchOption());
+  }
+
+  /**
+   * Adds the count option to the URL query.
+   *
+   * @param count the number of results to return
+   * @return the current instance of the builder
+   */
+  public BraveImageQueryBuilder count(int count) {
+    return addOptionCarrier(SearchOptions.count(count));
+  }
+
+  /**
+   * Adds the spell check option to the URL query.
+   *
+   * @param spellCheck whether to enable spellcheck
+   * @return the current instance of the builder
+   */
+  public BraveImageQueryBuilder spellcheck(boolean spellCheck) {
+    return addOptionCarrier(SearchOptions.spellCheck(spellCheck));
+  }
+}
