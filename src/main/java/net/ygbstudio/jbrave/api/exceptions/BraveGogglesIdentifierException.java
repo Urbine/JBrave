ec86cb87f460c383e96828c5ca5010b23647c6f8
Yoham Gabriel B. @Urbine <yohamg@programmer.net>
2026-01-02 17:50:30 +0700

    feat(exceptions): add BraveGogglesIdentifierException to handle invalid or malformed goggles URIs docs(exceptions): add Javadoc linking BraveGogglesIdentifierException to URL validation and MalformedURLException chore(license): add Apache-2.0 license header to BraveGogglesIdentifierException.java
    Signed-off-by: Yoham Gabriel B. @Urbine <yohamg@programmer.net>

---
+++ src/main/java/net/ygbstudio/jbrave/api/exceptions/BraveGogglesIdentifierException.java
@@ -0,0 +1,41 @@
+/*
+ * JBrave
+ *
+ * Copyright 2025-2026 Yoham Gabriel Barboza B. (YGBStudio)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package net.ygbstudio.jbrave.api.exceptions;
+
+import java.util.function.Supplier;
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * Exception that is thrown when a supplied goggles URI is not a valid URL candidate for its
+ * parameter value, for example, the resulting URL from URI is malformed.
+ *
+ * @see java.net.MalformedURLException
+ * @author Yoham Gabriel Barboza B. (YGBStudio)
+ */
+public class BraveGogglesIdentifierException extends RuntimeException {
+  public BraveGogglesIdentifierException(String message) {
+    super(message);
+  }
+
+  public BraveGogglesIdentifierException(@NotNull Supplier<String> message) {
+    super(message.get());
+  }
+}
