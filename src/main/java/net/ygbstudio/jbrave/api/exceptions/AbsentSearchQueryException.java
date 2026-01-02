9d3e7c48d788638068a89796b598d6ae0efff044
Yoham Gabriel B. @Urbine <yohamg@programmer.net>
2026-01-02 17:47:57 +0700

    feat(exceptions): add AbsentSearchQueryException to indicate missing search queries in URL builders docs(exceptions): add Javadoc explaining that AbsentSearchQueryException is thrown when queries are empty and reference AbstractQueryUrlBuilder chore(license): add Apache-2.0 license header to AbsentSearchQueryException.java
    Signed-off-by: Yoham Gabriel B. @Urbine <yohamg@programmer.net>

---
+++ src/main/java/net/ygbstudio/jbrave/api/exceptions/AbsentSearchQueryException.java
@@ -0,0 +1,42 @@
+/*
+ * JBrave
+ *
+ * Copyright 2025-2026 Yoham Gabriel Barboza B. (YGBStudio)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package net.ygbstudio.jbrave.api.exceptions;
+
+import java.util.function.Supplier;
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * Exception thrown when a search query is absent in a URL query builder method.
+ *
+ * <p>Queries cannot be empty and a request without it is not acceptable by the API.
+ *
+ * @see net.ygbstudio.jbrave.api.base.builders.AbstractQueryUrlBuilder
+ * @author Yoham Gabriel Barboza B. (YGBStudio)
+ */
+public class AbsentSearchQueryException extends RuntimeException {
+  public AbsentSearchQueryException(String message) {
+    super(message);
+  }
+
+  public AbsentSearchQueryException(@NotNull Supplier<String> message) {
+    super(message.get());
+  }
+}
