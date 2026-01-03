8c3e91b63508aaa3183b6d0f6239c3cb28d0212e
Yoham Gabriel B. @Urbine <yohamg@programmer.net>
2026-01-03 12:26:45 +0700

    refactor(api): update `Country`, `MarketLocale`, and `SearchLanguage` imports to use `options.provided` identifiers
    Signed-off-by: Yoham Gabriel B. @Urbine <yohamg@programmer.net>

--- src/main/java/net/ygbstudio/jbrave/api/codes/MarketLocale.java
+++ src/main/java/net/ygbstudio/jbrave/api/codes/MarketLocale.java
@@ -20,7 +20,7 @@
 
 package net.ygbstudio.jbrave.api.codes;
 
-import net.ygbstudio.jbrave.core.RegionLocaleIdentifier;
+import net.ygbstudio.jbrave.core.options.provided.RegionLocaleIdentifier;
 import net.ygbstudio.jbrave.core.model.SearchOptionCarrier;
 import net.ygbstudio.jbrave.api.options.BraveSearchOption;
 import org.jetbrains.annotations.Contract;
