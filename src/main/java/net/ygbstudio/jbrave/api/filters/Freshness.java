1a975ac4221f631744586c9307b9d74e61c4e95c
Yoham Gabriel B. @Urbine <yohamg@programmer.net>
2026-01-03 17:54:18 +0700

    refactor(Freshness): update import to use core.model.BraveSearchOption for internal consistency
    Signed-off-by: Yoham Gabriel B. @Urbine <yohamg@programmer.net>

--- src/main/java/net/ygbstudio/jbrave/api/filters/Freshness.java
+++ src/main/java/net/ygbstudio/jbrave/api/filters/Freshness.java
@@ -27,7 +27,7 @@ import net.ygbstudio.jbrave.core.domain.SearchFilterOption;
 import net.ygbstudio.jbrave.core.model.SearchOptionCarrier;
 import net.ygbstudio.jbrave.core.exceptions.InvalidFreshnessInterval;
 import net.ygbstudio.jbrave.core.domain.modes.SearchFilterMode;
-import net.ygbstudio.jbrave.api.options.BraveSearchOption;
+import net.ygbstudio.jbrave.core.model.BraveSearchOption;
 import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Unmodifiable;
