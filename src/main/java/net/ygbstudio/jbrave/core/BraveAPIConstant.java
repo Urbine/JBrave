775f8bfa47cafb3f3cd3180b8bb7baeb588dbfd6
Yoham Gabriel B. @Urbine <yohamg@programmer.net>
2026-01-03 12:28:22 +0700

    refactor(core): add `options.provided` identifiers to `BraveAPIConstant` imports
    Signed-off-by: Yoham Gabriel B. @Urbine <yohamg@programmer.net>

--- src/main/java/net/ygbstudio/jbrave/core/BraveAPIConstant.java
+++ src/main/java/net/ygbstudio/jbrave/core/BraveAPIConstant.java
@@ -24,6 +24,9 @@ import net.ygbstudio.jbrave.core.options.ClientProvidedOption;
 import net.ygbstudio.jbrave.core.options.ProvidedOption;
 import net.ygbstudio.jbrave.core.options.SearchFilterOption;
 import net.ygbstudio.jbrave.core.options.SearchOption;
+import net.ygbstudio.jbrave.core.options.provided.CountryIdentifier;
+import net.ygbstudio.jbrave.core.options.provided.LanguageIdentifier;
+import net.ygbstudio.jbrave.core.options.provided.RegionLocaleIdentifier;
 
 /**
  * Interface for constants that are used in Brave API.
