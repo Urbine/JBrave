b4da784f2343d3b116bd8eea24b0971f7c554d30
Yoham Gabriel B. @Urbine <yohamg@programmer.net>
2026-01-03 17:24:09 +0700

    refactor(SafeSearch): update SearchFilterMode import to core.domain.modes for consistency
    Signed-off-by: Yoham Gabriel B. @Urbine <yohamg@programmer.net>

--- src/main/java/net/ygbstudio/jbrave/api/filters/SafeSearch.java
+++ src/main/java/net/ygbstudio/jbrave/api/filters/SafeSearch.java
@@ -23,7 +23,7 @@ package net.ygbstudio.jbrave.api.filters;
 import net.ygbstudio.jbrave.core.domain.ClientProvidedOption;
 import net.ygbstudio.jbrave.core.domain.SearchFilterOption;
 import net.ygbstudio.jbrave.core.model.SearchOptionCarrier;
-import net.ygbstudio.jbrave.api.filters.modes.SearchFilterMode;
+import net.ygbstudio.jbrave.core.domain.modes.SearchFilterMode;
 import net.ygbstudio.jbrave.api.options.BraveSearchOption;
 import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.NotNull;
