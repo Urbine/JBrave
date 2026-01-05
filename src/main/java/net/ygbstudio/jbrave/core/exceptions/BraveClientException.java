ce5e19f6a801fc5009727b96187f1684301f1b6e
Yoham Gabriel B. @Urbine <yohamg@programmer.net>
2026-01-05 19:14:43 +0700

    feat(core.exceptions): add BraveClientException to represent client-side errors during API request execution
    Signed-off-by: Yoham Gabriel B. @Urbine <yohamg@programmer.net>

---
+++ src/main/java/net/ygbstudio/jbrave/core/exceptions/BraveClientException.java
@@ -0,0 +1,40 @@
+/*
+ * JBrave
+ *
+ * Copyright 2025-2026 Yoham Gabriel Barboza B. (YGBStudio)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package net.ygbstudio.jbrave.core.exceptions;
+
+import java.util.function.Supplier;
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * This exception is thrown when there is an error in a request involving the Brave Search API
+ * internal client. This exception is a runtime exception and it extends {@link RuntimeException}.
+ *
+ * @author Yoham Gabriel B. (YGBStudio)
+ */
+public class BraveClientException extends RuntimeException {
+  public BraveClientException(String message) {
+    super(message);
+  }
+
+  public BraveClientException(@NotNull Supplier<String> message) {
+    super(message.get());
+  }
+}
