ff9a7ca633d154a4e9f87bb2872b3728001dbc2e
Yoham Gabriel B. @Urbine <yohamg@programmer.net>
2026-01-06 17:58:21 +0700

    feat(core.executors): add asynchronous request execution with rate limiting and task list management
    Signed-off-by: Yoham Gabriel B. @Urbine <yohamg@programmer.net>

--- src/main/java/net/ygbstudio/jbrave/core/executors/AbstractRequestExecutor.java
+++ src/main/java/net/ygbstudio/jbrave/core/executors/AbstractRequestExecutor.java
@@ -24,10 +24,18 @@ import java.io.IOException;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Executors;
 import java.util.function.Supplier;
+import java.util.stream.Stream;
 import net.ygbstudio.jbrave.core.exceptions.BraveClientException;
+import net.ygbstudio.jbrave.core.exceptions.EmptyTaskListException;
+import net.ygbstudio.jbrave.api.options.BravePlan;
 import net.ygbstudio.jbrave.core.utils.GzipBodyHandler;
+import org.jetbrains.annotations.NotNull;
 
 /**
  * Abstract class for building the request and executing it.
@@ -38,6 +46,12 @@ import net.ygbstudio.jbrave.core.utils.GzipBodyHandler;
  * @param <T> The type of the concrete implementation of this abstract class.
  */
 public abstract class AbstractRequestExecutor<T extends AbstractRequestExecutor<T>> {
+
+  protected final HttpClient.Builder client =
+      HttpClient.newBuilder().version(HttpClient.Version.HTTP_2);
+  protected final GzipBodyHandler gzipBodyHandler = new GzipBodyHandler();
+  protected List<HttpRequest> requestList;
+  protected RateLimiter rateLimiter;
 
   /**
    * Returns the current instance of the builder.
@@ -54,14 +68,31 @@ public abstract class AbstractRequestExecutor<T extends AbstractRequestExecutor<
   }
 
   /**
+   * Adds a request to the list of requests.
+   *
+   * @param request the request to be added
+   * @return the current instance of the builder
+   */
+  protected final T addRequest(HttpRequest request) {
+    if (requestList == null) requestList = new ArrayList<>();
+    requestList.add(request);
+    return self();
+  }
+
+  protected final T addRateLimiter(RateLimiter limiter) {
+    this.rateLimiter = limiter;
+    return self();
+  }
+
+  /**
    * Executes the request and returns an optional response.
    *
    * @return an optional response to the request
    * @throws InterruptedException if the execution is interrupted
    */
-  public HttpResponse<String> execute(HttpRequest request) throws InterruptedException {
-    try (HttpClient client = HttpClient.newHttpClient()) {
-      return client.send(request, new GzipBodyHandler());
+  public final HttpResponse<String> execute(HttpRequest request) throws InterruptedException {
+    try (HttpClient httpClient = client.build()) {
+      return httpClient.send(request, new GzipBodyHandler());
     } catch (IOException ioEx) {
       Supplier<String> clientEx =
           () ->
@@ -69,6 +100,35 @@ public abstract class AbstractRequestExecutor<T extends AbstractRequestExecutor<
                   + request.uri().toString()
                   + (Objects.nonNull(ioEx.getCause()) ? " Caused by: " + ioEx.getCause() : "");
       throw new BraveClientException(clientEx);
+    }
+  }
+
+  /**
+   * Executes the request asynchronously and returns a stream of CompletableFutures, each containing
+   * an optional response to the request.
+   *
+   * @return a stream of CompletableFutures, each containing an optional response to the request
+   * @throws EmptyTaskListException if the task list is empty
+   */
+  protected final @NotNull Stream<CompletableFuture<HttpResponse<String>>> executeAsync() {
+    RateLimiter limiter =
+        Objects.nonNull(rateLimiter) ? rateLimiter : RateLimiter.of(BravePlan.FREE);
+    try (HttpClient httpClient =
+        client
+            .executor(
+                CompletableFuture.delayedExecutor(
+                    limiter.delay(),
+                    limiter.timeUnit(),
+                    Executors.newVirtualThreadPerTaskExecutor()))
+            .build()) {
+      if (requestList == null) {
+        Supplier<String> taskListErr =
+            () -> "Unable to execute tasks asynchronously. Task list is empty.";
+        throw new EmptyTaskListException(taskListErr);
+      }
+      return requestList.parallelStream()
+          .unordered()
+          .map(req -> httpClient.sendAsync(req, gzipBodyHandler));
     }
   }
 }
