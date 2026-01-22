d5ffcb190ae905546ea91a1704d7f94c35867f8e
Yoham Gabriel B. @Urbine <yohamg@programmer.net>
2026-01-22 11:24:47 +0700

    feat(model): introduce BraveErrorCode enum to represent API error codes
    Signed-off-by: Yoham Gabriel B. @Urbine <yohamg@programmer.net>

---
+++ src/main/java/net/ygbstudio/jbrave/core/model/BraveErrorCode.java
@@ -0,0 +1,46 @@
+/*
+ * JBrave
+ *
+ * Copyright 2025-2026 Yoham Gabriel Barboza B. (YGBStudio)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package net.ygbstudio.jbrave.core.model;
+
+/**
+ * Enumeration of Brave API error codes.
+ *
+ * <p>This enum represents the different error codes that the Brave API can return.
+ *
+ * @author Yoham Gabriel @ YGB Studio
+ */
+public enum BraveErrorCode {
+  INTERNAL,
+  QUOTA_LIMITED,
+  RATE_LIMITED,
+  SUBSCRIPTION_TOKEN_INVALID,
+  SUBSCRIPTION_NOT_FOUND,
+  RESOURCE_NOT_ALLOWED,
+  OPTION_NOT_IN_PLAN,
+  USAGE_LIMIT_EXCEEDED,
+  INVALID_URL,
+  VALIDATION;
+
+  @Override
+  public String toString() {
+    return this.name();
+  }
+}
