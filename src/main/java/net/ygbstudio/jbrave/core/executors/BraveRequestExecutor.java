39c4571755ca31b989289bc6a2d2ea22399bd5b2
Yoham Gabriel B. @Urbine <yohamg@programmer.net>
2026-01-26 18:51:16 +0700

    refactor(executors): remove batch-oriented APIs from BraveRequestExecutor to align with single-request execution model
    Signed-off-by: Yoham Gabriel B. @Urbine <yohamg@programmer.net>

--- src/main/java/net/ygbstudio/jbrave/core/executors/BraveRequestExecutor.java
+++ src/main/java/net/ygbstudio/jbrave/core/executors/BraveRequestExecutor.java
@@ -20,11 +20,6 @@
 
 package net.ygbstudio.jbrave.core.executors;
 
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
-import java.util.function.Function;
-import java.util.function.Supplier;
-import java.util.stream.Stream;
 import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.NotNull;
 
@@ -45,34 +40,5 @@ public final class BraveRequestExecutor extends AbstractRequestExecutor<BraveReq
   @Contract(" -> new")
   public static @NotNull BraveRequestExecutor getInstance() {
     return new BraveRequestExecutor();
-  }
-
-  /**
-   * Submits a request to the request list.
-   *
-   * @param requestSupplier the request to be added
-   * @return the current instance of the builder
-   */
-  public BraveRequestExecutor submitTask(Supplier<HttpRequest> requestSupplier) {
-    return submit(requestSupplier);
-  }
-
-  /**
-   * Returns a {@link Stream} of {@link HttpResponse}s, in request order.
-   *
-   * @return a {@link Stream} of {@link HttpResponse}s, in request order
-   */
-  public @NotNull Stream<HttpResponse<String>> responseStream() {
-    return executeAllPaced(Function.identity()).stream();
-  }
-
-  /**
-   * Executes the request and returns a single {@link HttpResponse} as a string.
-   *
-   * @param requestSupplier the request to be executed
-   * @return a single {@link HttpResponse} as a string
-   */
-  public HttpResponse<String> executeStringResponseOnce(Supplier<HttpRequest> requestSupplier) {
-    return submitTask(requestSupplier).executeAllPaced(Function.identity()).getFirst();
   }
 }
